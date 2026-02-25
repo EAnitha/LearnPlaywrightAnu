2b4586c44e6994397fa47180ed8ae1acafe3f152
Anitha123256 <anithaanu0463@gmail.comm>
2026-02-25 11:01:00 +0530

    data driven

--- tests/addemployee.spec.js
+++ tests/addemployee.spec.js
@@ -2,63 +2,77 @@ import { test, expect } from '@playwright/test';
 import logindata from "../testdata/login.json"
 //import addempdata from "../testdata/PIM/addemployee.json" // json data import
 //import { faker } from '@faker-js/faker';
-import ExcelJS from 'exceljs'; // from excel data import
+//import ExcelJS from 'exceljs'; // from excel data import
+
+const employees = { // data driven testing using json
+  emp1: { firstName: "Anitha", lastName: "E" },
+  emp2: { firstName: "Anu", lastName: "M" },
+  emp3: { firstName: "Ammu", lastName: "E" }
+}
+for (let emp in employees) {
+  test(`verify admin can create employee with mandatory fields: ${employees[emp].firstName} ${employees[emp].lastName}`, async ({ page }) => {
+    await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+    await page.getByRole('textbox', { name: 'Username' }).click();
+    await page.getByRole('textbox', { name: 'Username' }).fill(logindata.username);
+    await page.getByRole('textbox', { name: 'Password' }).click();
+    await page.getByRole('textbox', { name: 'Password' }).fill(logindata.password);
+    await page.getByRole('button', { name: 'Login' }).click();
+    await expect(page.getByRole('heading', { name: 'Dashboard' })).toBeVisible();
+    await page.getByRole('link', { name: 'PIM' }).click();
+    await page.getByRole('listitem').filter({ hasText: 'Add Employee' }).click();
+    await page.getByRole('textbox', { name: 'First Name' }).click();
+    // using json data for data generation
+    // await page.getByRole('textbox', { name: 'First Name' }).fill(addempdata.firstname);
+    // await page.getByRole('textbox', { name: 'Last Name' }).click();
+    // await page.getByRole('textbox', { name: 'Last Name' }).fill(addempdata.lastname);
+
+    // using faker json for data generation
+    // await page.getByRole('textbox', { name: 'First Name' }).fill(faker.person.firstName());
+    // await page.getByRole('textbox', { name: 'Last Name' }).click();
+    // await page.getByRole('textbox', { name: 'Last Name' }).fill(faker.person.lastName());
+
+    // own random data generation
+    // let randomchar = (Math.random() + 1).toString(36).substring(7); //5 char generation
+    // await page.getByRole('textbox', { name: 'First Name' }).fill("Anu" + randomchar);
+    // await page.getByRole('textbox', { name: 'Last Name' }).click();
+    // await page.getByRole('textbox', { name: 'Last Name' }).fill("E" + randomchar);
 
 
-test('verify admin can create employee with mandatory fields', async ({ page }) => {
-  await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
-  await page.getByRole('textbox', { name: 'Username' }).click();
-  await page.getByRole('textbox', { name: 'Username' }).fill(logindata.username);
-  await page.getByRole('textbox', { name: 'Password' }).click();
-  await page.getByRole('textbox', { name: 'Password' }).fill(logindata.password);
-  await page.getByRole('button', { name: 'Login' }).click();
-  await expect(page.getByRole('heading', { name: 'Dashboard' })).toBeVisible();
-  await page.getByRole('link', { name: 'PIM' }).click();
-  await page.getByRole('listitem').filter({ hasText: 'Add Employee' }).click();
-  await page.getByRole('textbox', { name: 'First Name' }).click();
-  // using json data for data generation
-  // await page.getByRole('textbox', { name: 'First Name' }).fill(addempdata.firstname);
-  // await page.getByRole('textbox', { name: 'Last Name' }).click();
-  // await page.getByRole('textbox', { name: 'Last Name' }).fill(addempdata.lastname);
+    // using excel data
+    //   const workbook = new ExcelJS.Workbook();
+    //   await workbook.xlsx.readFile('./testdata/addemp.xlsx');
 
-  // using faker json for data generation
-  // await page.getByRole('textbox', { name: 'First Name' }).fill(faker.person.firstName());
-  // await page.getByRole('textbox', { name: 'Last Name' }).click();
-  // await page.getByRole('textbox', { name: 'Last Name' }).fill(faker.person.lastName());
+    //   const worksheet = workbook.getWorksheet('Sheet1'); // or workbook.worksheets[0]
 
-  // own random data generation
-  // let randomchar = (Math.random() + 1).toString(36).substring(7); //5 char generation
-  // await page.getByRole('textbox', { name: 'First Name' }).fill("Anu" + randomchar);
-  // await page.getByRole('textbox', { name: 'Last Name' }).click();
-  // await page.getByRole('textbox', { name: 'Last Name' }).fill("E" + randomchar);
-  
+    //   // Get header row
+    //   const headerRow = worksheet.getRow(1);
 
-  // using excel data
-  const workbook = new ExcelJS.Workbook();
-  await workbook.xlsx.readFile('./testdata/addemp.xlsx');
+    //   // Get data row (row 2)
+    //   const dataRow = worksheet.getRow(2);
 
-  const worksheet = workbook.getWorksheet('Sheet1'); // or workbook.worksheets[0]
+    //   const firstName = dataRow.getCell(1).value?.toString();
+    //   const lastName = dataRow.getCell(2).value?.toString();
 
-  // Get header row
-  const headerRow = worksheet.getRow(1);
+    //   console.log(firstName); // Anitha
+    //   console.log(lastName); // E
 
-  // Get data row (row 2)
-  const dataRow = worksheet.getRow(2);
 
-  const firstName = dataRow.getCell(1).value?.toString();
-  const lastName = dataRow.getCell(2).value?.toString();
+    // // excel data usage
+    //   await page.getByRole('textbox', { name: 'First Name' }).fill(firstName);
+    //   await page.getByRole('textbox', { name: 'Last Name' }).click();
+    //   await page.getByRole('textbox', { name: 'Last Name' }).fill(lastName);
 
-  console.log(firstName); // Anitha
-  console.log(lastName); // E
+    await page.getByRole('textbox', { name: 'First Name' }).fill(employees[emp].firstName);
+    await page.getByRole('textbox', { name: 'Last Name' }).click();
+    await page.getByRole('textbox', { name: 'Last Name' }).fill(employees[emp].lastName);
 
-  
-// excel data usage
-  await page.getByRole('textbox', { name: 'First Name' }).fill(firstName);
-  await page.getByRole('textbox', { name: 'Last Name' }).click();
-  await page.getByRole('textbox', { name: 'Last Name' }).fill(lastName);
-  
-  await page.waitForTimeout(4000)
-  await page.getByRole('button', { name: 'Save' }).click();
-  await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/pim/viewPersonalDetails/empNumber/235');
-  await expect(page.locator("//h6[text()='Personal Details']")).toBeVisible();
-});
+    await page.waitForTimeout(4000)
+    await page.getByRole('button', { name: 'Save' }).click();
+
+    // Wait until URL contains viewPersonalDetails
+    await expect(page).toHaveURL(/viewPersonalDetails/);
+
+    // Validate heading
+    await expect(page.getByRole('heading', { name: 'Personal Details' })).toBeVisible();
+  });
+} // End of for loop
--- tests/addjobtitle.spec.js
+++ tests/addjobtitle.spec.js
@@ -1,13 +1,18 @@
 import { test, expect } from '@playwright/test';
-import { faker } from '@faker-js/faker';
+//import { faker } from '@faker-js/faker';
+const jobTitles = {
+  jobTitle1: "SDE",
+  jobTitle2: "QA",
+  jobTitle3: "BA"
 
-
-test('Verify admin can add job titles', async ({ page }) => {
+}
+for(let jobTitle in jobTitles){
+test(`Verify admin can add job title: ${jobTitles[jobTitle]}`, async ({ page }) => {
   await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
   await page.getByRole('textbox', { name: 'Username' }).click();
   await page.getByRole('textbox', { name: 'Username' }).fill(process.env.APP_USERNAME);
   await page.getByRole('textbox', { name: 'Password' }).click();
-  await page.getByRole('textbox', { name: 'Password' }).fill(process.env.APP_PSSSWORD);
+  await page.getByRole('textbox', { name: 'Password' }).fill(process.env.APP_PASSWORD);
   await page.getByRole('button', { name: 'Login' }).click();
   await expect(page.getByRole('heading', { name: 'Dashboard' })).toBeVisible();
   await page.getByRole('link', { name: 'Admin' }).click();
@@ -17,12 +22,13 @@ test('Verify admin can add job titles', async ({ page }) => {
   await page.getByRole('textbox').nth(1).click();
   //let randomchar = (Math.random() + 1).toString(36).substring(7); //5 char generation
 
-  await page.getByRole('textbox').nth(1).fill(faker.person.jobTitle()); // using faker json
-  await page.waitForTimeout(4000)
-  await page.getByRole('textbox', { name: 'Type description here' }).click();
-  await page.getByRole('textbox', { name: 'Type description here' }).fill('Automation Tester');
-  await page.getByRole('textbox', { name: 'Add note' }).click();
-  await page.getByRole('textbox', { name: 'Add note' }).fill('Notes');
-  await page.getByRole('button', { name: 'Save' }).click();
+  await page.getByRole('textbox').nth(1).fill(jobTitles[jobTitle]); // using faker json
+  // await page.waitForTimeout(4000)
+  // await page.getByRole('textbox', { name: 'Type description here' }).click();
+  // await page.getByRole('textbox', { name: 'Type description here' }).fill('Automation Tester');
+  // await page.getByRole('textbox', { name: 'Add note' }).click();
+  // await page.getByRole('textbox', { name: 'Add note' }).fill('Notes');
+   await page.getByRole('button', { name: 'Save' }).click();
   await expect(page.getByRole('heading', { name: 'Job Titles' })).toBeVisible({timeout: 30000});
 });
+}
